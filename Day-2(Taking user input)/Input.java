fc3ecd941dee24954d5fc559fb8c1d74af81499d
Sumit Singh Thakur <sumit942538@gmail.com>
2025-07-04 12:41:54 +0530

    Toady i learned in java that how to take user input and feeling sick today so not continue this further

---
+++ Day-2(Taking user input)/Input.java
@@ -0,0 +1,69 @@
+// Today we are going to learn about the how to take input and how to print them and how to take input from differnt data type:
+
+// "THis is how we take input with string and perform operations "
+
+// import java.util.Scanner;
+// class ip{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter your name here:- ");
+//         String str = sc.nextLine();
+//         System.out.println(str);
+//         sc.close();
+//     }
+// }
+
+// "THis is how we take input as int and float and perform operation"
+
+// import java.util.Scanner;
+
+// class ip{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner (System.in);
+//         System.out.println("Enter number here:- ");
+//         int a = sc.nextInt();
+//         System.out.println("Enter number here:- ");
+//         int b = sc.nextInt();
+//         int c = a+b ;
+//         System.out.println(c);
+//         sc.close();
+//     }
+// }
+
+
+// import java.util.Scanner;
+
+// class ip{
+//     public static void main(String[] args){
+//         Scanner sc =new Scanner(System.in);
+//         System.out.println("Enter the number here:- ");
+//         float a = sc.nextFloat();
+//         System.out.println("Enter the number here:- ");
+//         float b = sc.nextFloat();
+//         float c =  a + b ;
+//         System.out.println(c);
+//         sc.close();
+//     }
+// }
+
+// SOme questions or program related to this 
+
+// import java.util.Scanner;
+// class ip{
+//     public static void main(String[] args){
+//         System.out.println("Mathematics");
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter the first number here:- ");
+//         int a = sc.nextInt();
+//         System.out.println("Enter the Second number here:- ");
+//         int b = sc.nextInt();
+//         int result = a +b ;
+//         int result = a - b ;
+//         int result = a *b ;
+//         int result = a / b ;
+//         System.out.println(result);
+//         sc.close();
+//     }
+// }
+
+
